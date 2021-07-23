9ecfe2a5bbded43354b80bac04ea09b3e6c1f6d7
Elise Andro <elise.andro@gmail.com>
2021-07-23 11:30:07 +0200

    ajout de fichiers

--- coursoc/app.py
+++ coursoc/app.py
@@ -1,25 +1,18 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def p5():
+    return render_template('projet5.html')
 
-@app.route("/dashboard/")
-def dashboard():
-    return render_template('dashboard.html')
-
-@app.route('/api/meteo/')
-def meteo():
-    dictionnaire = {
-        'type': 'Prévision de température',
-        'valeurs': [24, 24, 25, 26, 27, 28],
-        'unite': "degrés Celcius"
-    }
-    return jsonify(dictionnaire)
-
+@app.route("/api/", methods = ["POST"])
+def tagapi():
+    titre = request.form.get('titre')
+    contenu = request.form.get('contenu')
+    print(titre, contenu)
+    return jsonify({'tags': ['exemple']})
 
 if __name__ == "__main__":
     app.run(debug=True)
