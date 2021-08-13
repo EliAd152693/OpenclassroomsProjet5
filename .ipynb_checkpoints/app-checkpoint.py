723d305683cd915e32776e18ccb0c60970444da6
Elise Andro <elise.andro@gmail.com>
2021-08-13 15:11:48 +0200

    api fonctionne

--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
 from flask import Flask, request, jsonify, render_template
+from wtforms import Form, TextAreaField, validators
 import joblib
 import json
 import pandas as pd
@@ -10,22 +10,45 @@ from gensim.utils import simple_preprocess
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn import preprocessing
 
-app = Flask(__name__)
-
-@app.route("/api/", methods = ["POST"])
-def predict():
-    data = request.get_json()
-    tokenized_input = gensim.utils.simple_preprocess(data, deacc=True)
+def predict_tag(document):
+    tokenized_input = gensim.utils.simple_preprocess(document, deacc=True)
     tfidf_input = tfidf.transform(tokenized_input)
     num_pred = model.predict(tfidf_input)
     prediction = np.array(encoder.inverse_transform(num_pred))
-    return jsonify(np.array2string(prediction))
+    return np.array2string(prediction)
+
+
+app = Flask(__name__)
+
+class QuestionForm(Form):
+    sentence = TextAreaField('', 
+                                [validators.DataRequired(), validators.length(min=15)])
+
+    
+@app.route("/")
+def index():
+    form = QuestionForm(request.form)
+    return render_template('questionform.html', form=form)
+
+
+@app.route("/results", methods = ["POST"])
+def results():
+    form = QuestionForm(request.form)
+    if request.method == 'POST' and form.validate():
+        data = request.form.get('sentence')
+        pred = predict_tag(data)
+        return render_template('results.html',
+                              content = data,
+                              prediction = pred)
+
+    return render_template('questionform.html', form=form)
+
 
 if __name__ == "__main__":
-    nlp_model = open('model.pkl', 'rb')
+    nlp_model = open('pkl_objects/model.pkl', 'rb')
     model = joblib.load(nlp_model)
-    tfidf_model = open('tfidf.pkl', 'rb')
+    tfidf_model = open('pkl_objects/tfidf.pkl', 'rb')
     tfidf = joblib.load(tfidf_model)
-    encoder_model = open('encoder.pkl', 'rb')
+    encoder_model = open('pkl_objects/encoder.pkl', 'rb')
     encoder = joblib.load(encoder_model)
     app.run(debug=True)
