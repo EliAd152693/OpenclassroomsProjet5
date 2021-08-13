4898a34579501047276b744611b327cf3d7677f2
Elise Andro <elise.andro@gmail.com>
2021-08-13 15:30:37 +0200

    requirements update

--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 from wtforms import Form, TextAreaField, validators
 import joblib
-import json
 import pandas as pd
 import numpy as np
 import pickle
